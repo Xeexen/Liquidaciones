92be3be18c5f6198345277fa46aabb4f412e90d0
AndresO <andres.o.mm@hotmail.com>
2024-07-18 15:15:45 -0500

    Init Commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import CalculadoraLiquidaciones from './components/CalculadoraLiquidaciones.vue'
+</script>
+
+<template>
+  <div>
+    <CalculadoraLiquidaciones />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/CalculadoraLiquidaciones.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <h1 class="title">Calculadora de Liquidaciones</h1>
+    </div>
+    <div>
+        <table class="table">
+            <thead>
+                <th>Calculadora</th>
+            </thead>
+        </table>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue';
+
+
+interface liquidacion {
+    liquidacion: number;
+    mes: number;
+}
+
+const liquidaciones = ref([])
+
+const calc = () => {
+
+}
+</script>
+
+<style scoped></style>
