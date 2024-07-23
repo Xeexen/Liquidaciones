09b081cfa25ac92543c8795da4b0ca355cb0933c
PROPHET2350 <jorgeluis33ariosa@gmail.com>
2024-07-23 15:29:57 -0500

    something

---
+++ src/models/models.ts
@@ -0,0 +1,16 @@
+export interface ContractsType {
+  code: string;
+  name: string;
+  rate: number;
+}
+
+export interface Contract {
+  region: string;
+  firstDate: string;
+  lastDate: string;
+  contractType: ContractsType;
+  lastPay: number;
+  vacations: number;
+  decimoTercero: false;
+  decimoCuarto: false;
+}
